3fd3dbfa6d0d2b6d432ab951a10b9796100de148
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2026-05-10 05:14:06 +0530

    fix: add deprecation warning filter

--- src/agentic_rag/__init__.py
+++ src/agentic_rag/__init__.py
@@ -1,3 +1,16 @@
+import warnings
+
+from langchain_core._api.deprecation import LangChainPendingDeprecationWarning
+
 __all__ = ["__version__"]
 
 __version__ = "0.1.0"
+
+warnings.filterwarnings(
+    "ignore",
+    message=(
+        r"The default value of `allowed_objects` will change in a future version\..*"
+    ),
+    category=LangChainPendingDeprecationWarning,
+    module=r"langgraph\.cache\.base(\.__init__)?",
+)
