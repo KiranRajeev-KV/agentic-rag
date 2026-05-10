8f0018444959c4e3001341fe50097ed50adc4b6f
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2026-05-10 12:12:06 +0530

    chore(format): normalize warnings filter formatting

--- src/agentic_rag/__init__.py
+++ src/agentic_rag/__init__.py
@@ -8,9 +8,7 @@ __version__ = "0.1.0"
 
 warnings.filterwarnings(
     "ignore",
-    message=(
-        r"The default value of `allowed_objects` will change in a future version\..*"
-    ),
+    message=(r"The default value of `allowed_objects` will change in a future version\..*"),
     category=LangChainPendingDeprecationWarning,
     module=r"langgraph\.cache\.base(\.__init__)?",
 )
